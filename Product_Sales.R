f41161d7bc54dc181395df59869f40615794ae3e
hsaripalli <45947172+hsaripalli@users.noreply.github.com>
2021-04-12 17:35:37 -0600

    Updated plot

--- Product_Sales.R
+++ Product_Sales.R
@@ -25,4 +25,31 @@ sales$Product_ID <-str_pad(as.character(sales$Product_ID),3)
 str(sales$Product_ID)
 nchar(sales$Product_ID)
 
+#tidy data
 str(sales)
+tidy_sales <- gather(sales, year, sales_by_year, Sales_2016, Sales_2017)
+tidy_sales$year <- as.numeric(str_remove(tidy_sales$year, "Sales_"))
+str(tidy_sales$year)
+
+
+theme_set(theme_bw())  # pre-set the bw theme.
+
+
+plot1 <- ggplot(tidy_sales, aes(Price)) + 
+  geom_histogram(binwidth = 5, color = "black", fill = "cornflowerblue") + 
+  labs(title = "Price Distribution", x = "Price, $",  y = "Observations") + 
+  scale_x_continuous(breaks = seq(0,80,5))
+plot1
+
+plot2 <- ggplot(tidy_sales, aes(x =Price, y = sales_by_year, color = factor(year))) + 
+  geom_point() + 
+  labs(title = "Total sales versus Price", x = "Price, $",  y = "Total Sales, $", color = "Year")
+plot2
+
+plot3 <- ggplot(tidy_sales, aes(x = Num_Retailers, y =Price )) + 
+  geom_point() + 
+  labs(title = "Price versus number of retailers", x = "No. of retailers", y = "Price, $")
+plot3
+
+cor <- cor(tidy_sales$Price, tidy_sales$Num_Retailers)
+cor
