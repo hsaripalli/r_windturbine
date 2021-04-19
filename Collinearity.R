11d097051943504b881347287501c01a6c56d337
hsaripalli <45947172+hsaripalli@users.noreply.github.com>
2021-04-19 10:19:55 -0600

    rev1

--- Collinearity.R
+++ Collinearity.R
@@ -1,51 +1,57 @@
+
+# Load libraries 
+
 library(readxl)
-library(dplyr)
 library(ggplot2)
-library(lubridate)
-library(stringr)
 
-
-# Import sheet Product_Sales
+# Import data from the "Collinearity" tab into a data frame 
 
 coll_data <- read_excel("C:\\hS\\GMMA\\860 Acquistion and Management of Data\\Assignment 1\\GMMA860_Assignment1_Data_vf.xlsx", 
-                    sheet = "Collinearity")
+                        sheet = "Collinearity")
+
+#Examine data
 
 head(coll_data)
 str(coll_data)
 summary(coll_data)
 
+#Filter first 25 observations
 filtered_data <-filter(coll_data, coll_data$Obs<26)
-filtered_data
 
-?lm
-
-
-#Y in terms of exp and height: 1st 25 observations
+#Run regression for Y in terms of experience and height for the first 25 observations
 
 reg1 <- lm(Y ~ Experience + Height, filtered_data)
 summary(reg1)
-plot(reg1)
 
-#Y in terms of exp and height: entire data set
-reg2 <- lm(Y ~ Experience + Height, coll_data)
+
+#Run regression for Y in terms of experience and weight for the first 25 observations
+reg2 <- lm(Y ~ Experience + Weight, filtered_data)
 summary(reg2)
-plot(reg2)
 
-#Y in terms of exp and weight: 1st 25 observations
-reg3 <- lm(Y ~ Experience + Weight, filtered_data)
-reg3
-
-#Y in terms of exp and weight: entire data set
-reg4 <- lm(Y ~ Experience + Weight, coll_data)
-reg4
+# Run regression for Y in terms of experience, height and weight for the first 25 observations
+reg3 <- lm(Y ~ Experience + Height + Weight, filtered_data)
+summary(reg3)
 
 
-#Y in terms of exp, height and weight: first 25 observations
+#Y in terms of experience and height: entire data set
+reg4 <- lm(Y ~ Experience + Height, coll_data)
+summary(reg4)
 
-reg5 <- lm(Y ~ Experience + Height + Weight, filtered_data)
-reg5
+
+#Y in terms of experience and weight: entire data set
+reg5 <- lm(Y ~ Experience + Weight, coll_data)
+summary(reg5)
 
 
-#Y in terms of exp, height and weight: entire data set
+#Y in terms of experience, height and weight: entire data set
 reg6 <- lm(Y ~ Experience + Height + Weight, coll_data)
-reg6
+summary(reg6)
+
+plot <- ggplot(filtered_data, aes(x = Experience, y = Y)) + 
+  geom_point() + 
+  ggtitle("Correlation between Weight and Height")
+plot
+
+
+
+
